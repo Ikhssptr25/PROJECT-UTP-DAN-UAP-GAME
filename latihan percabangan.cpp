d54e61f62b22e00b6efb729155d086fb72322be8
Ikhsan saputra <92314859+Ikhssptr25@users.noreply.github.com>
2021-11-17 15:25:55 +0700

    Add files via upload

---
+++ latihan percabangan.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	
+	/*ikhsan saputra
+	  2117051037
+	  kelas B*/
+	  
+   	
+	// deklarasi variabel//
+	int gaji_karyawan;
+	int UMR_daerah ;
+	
+	//input data
+	cout<<"GAJI KARYAWAN :"; cin>>gaji_karyawan;
+	cout<<"UMR DAERAH    :"; cin>>UMR_daerah;
+	
+	// percabangan output
+	 if(gaji_karyawan<UMR_daerah)
+	{cout<<"GAJI ANDA DIBAWAH UMR DAERAH"<<endl;
+	}else if (gaji_karyawan==UMR_daerah) 
+	{cout<<"GAJI ANDA SETARA UMR DAERAHH"<<endl;
+	}else if(gaji_karyawan>UMR_daerah)
+	{cout<<"GAJI ANDA DIATAS UMR DAERAH"<<endl;
+	}
+	
+	return 0;
+}
